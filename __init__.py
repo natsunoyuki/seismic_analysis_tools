944e73c814f9599ee2bfab55fbd409ce3b8aa590
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2021-01-27 00:03:23 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
 # seismic_analysis_tools
+
+# 1. Sac.py
+#    Functions to read .sac files, and to handle them using pandas.
+# 2. ymaeda_tools
+#    A set of python codes associated with the linear inversion C code written originally by Dr. Yuta Maeda of Nagoya University.
