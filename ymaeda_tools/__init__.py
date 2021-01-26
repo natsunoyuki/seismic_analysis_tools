29230de3d8241b920a4a3bf07a699621191caa0d
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2021-01-26 23:58:09 +0900

    Add files via upload

--- ymaeda_tools/__init__.py
+++ ymaeda_tools/__init__.py
@@ -1,4 +1,17 @@
 # ymaeda_tools
-# This set of tools are meant to be used with the 
-# seismic inversion programs written by Dr. Yuta Maeda
-# of Nagoya university
+
+# This set of tools are meant to be used with the seismic inversion programs written by Dr. Yuta Maeda of Nagoya university.
+# The original set of programs are referred to as YMAEDA_TOOLS which is written in C.
+
+# 1. dft.py
+#    Functions associated with the discrete Fourier transform functions used in the original C code.
+# 2. inversion.py
+#    Functions associated with the linear inversion algorithm used in the original C code.
+# 3. polezero.py
+#    Functions associated with poles and zeros (seismometer response) used in the original C code.
+# 4. read_dGM.py
+#    Functions associated with reading the d, G and M .seq files created by the original C code.
+# 5. read_snapshot.py
+#    Functions associated with reading the "effective Green's functions" snapshot .3db files created by the original C code.
+# 6. read_stfun.py
+#    Functions associated with the source time functions used in the original C code.
