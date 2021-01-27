d18745a67cba1d9aa1a1cb04a152e5cef76462f6
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2021-01-27 22:38:16 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,17 +1,21 @@
 # seismic_analysis_tools
 
-# Python Files
-# 1. Sac.py
-#    Functions to read .sac files, and to handle them using pandas in python.
-
 # Directories
-# 1. ymaeda_tools
+# -----------
+# 1. sac_tools
+#    A set of python codes which uses the obspy library to deal with .sac data. In particular, the codes in this directory allow for quick conversion to pd.DataFrames for a more pythonic seismic data analysis.
+# 2. ymaeda_tools
 #    A set of python codes associated with the linear inversion C code (YMAEDA_TOOLS) written originally by Dr. Yuta Maeda of Nagoya University.
 
 # Jupyter Notebooks
-# 1. ymaeda_tools_gf_checks.ipynb
+# -----------------
+# 1. Sac_test.ipynb
+#    Jupyter notebook containing tests of the Sac.py codes in sac_tools.
+# 2. Shinmoedake.ipynb
+#    Jupyter notebook containing tests of the Shinmoedake.py codes in sac_tools.
+# 3. ymaeda_tools_gf_checks.ipynb
 #    Jupyter notebook containing checks of the Green's functions calculated by runwaterPML.
-# 2. ymaeda_tools_inv_checks.ipynb
+# 4. ymaeda_tools_inv_checks.ipynb
 #    Jupyter notebook containing checks of the linear inversion results calculated by winv.
-# 3. ymaeda_tools_dGM_check.ipynb
+# 5. ymaeda_tools_dGM_check.ipynb
 #    Jupyter notebook containing checks of the d_obs.cv, m_est.cv and G.dbm files created by winv.
