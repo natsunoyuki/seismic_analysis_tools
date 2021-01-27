08cd9e8f70874c026f91716dcfa7b0fa00d1cda3
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2021-01-27 16:40:47 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,6 +1,18 @@
 # seismic_analysis_tools
 
+
+# Python Files
 # 1. Sac.py
-#    Functions to read .sac files, and to handle them using pandas.
-# 2. ymaeda_tools
-#    A set of python codes associated with the linear inversion C code written originally by Dr. Yuta Maeda of Nagoya University.
+#    Functions to read .sac files, and to handle them using pandas in python.
+
+# Directories
+# 1. ymaeda_tools
+#    A set of python codes associated with the linear inversion C code (YMAEDA_TOOLS) written originally by Dr. Yuta Maeda of Nagoya University.
+
+# Jupyter Notebooks
+# 1. ymaeda_tools_gf_checks.ipynb
+#    Jupyter notebook containing checks of the Green's functions calculated by runwaterPML.
+# 2. ymaeda_tools_inv_checks.ipynb
+#    Jupyter notebook containing checks of the linear inversion results calculated by winv.
+# 3. ymaeda_tools_dGM_check.ipynb
+#    Jupyter notebook containing checks of the d_obs.cv, m_est.cv and G.dbm files created by winv.
